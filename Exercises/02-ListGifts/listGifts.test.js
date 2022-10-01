64480bb9847d742133f96d69a97a5dc01384eadb
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-10-01 05:20:03 -0400

    feat(listGifts): add edge case test for exercise

--- Exercises/02-ListGifts/listGifts.test.js
+++ Exercises/02-ListGifts/listGifts.test.js
@@ -14,3 +14,18 @@ test('can list gift', () => {
 
   expect(result).toStrictEqual(expected);
 });
+
+test('can list gift with multiple whitespaces', () => {
+  const letter = 'bike   car   ball _playstation  bike car  teddy ';
+
+  const expected = {
+    bike: 2,
+    car: 2,
+    ball: 1,
+    teddy: 1
+  };
+
+  const result = listGifts(letter);
+
+  expect(result).toStrictEqual(expected);
+});
