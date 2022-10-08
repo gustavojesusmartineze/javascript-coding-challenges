15e8ee528016f40f5a18955cdb5c30993d558fbf
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-10-08 07:06:55 -0400

    feat(maxProfit): add tests for exercise

---
+++ Exercises/08-MaxProfit/maxProfit.test.js
@@ -0,0 +1,20 @@
+const { maxProfit } = require('./maxProfit');
+const { expect } = require('@jest/globals');
+
+test('should return 16', () => {
+  const input = [39, 18, 29, 25, 34, 32, 5];
+  const expected = 16; // -> 16 (buys at 18, sells at 34)
+
+  const result = maxProfit(input);
+
+  expect(result).toBe(expected);
+});
+
+test('should return 60', () => {
+  const input = [10, 20, 30, 40, 50, 60, 70];
+  const expected = 60; // -> 60 (buys at 10, sells at 70)
+
+  const result = maxProfit(input);
+
+  expect(result).toBe(expected);
+});
