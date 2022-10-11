de987684f34cc11cb8ca43569ec6ad006f004fc0
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-10-11 14:48:39 -0400

    feat(missingReindeer): add tests for exercise

---
+++ Exercises/14-MissingReindeer/missingReindeer.test.js
@@ -0,0 +1,42 @@
+const { missingReindeer } = require('./missingReindeer');
+const { expect } = require('@jest/globals');
+
+test('should return 1', () => {
+  const input = [0, 2, 3];
+
+  const expected = 1; 
+  
+  const result = missingReindeer(input);
+
+  expect(result).toBe(expected);
+});
+
+test('should return 4', () => {
+  const input = [5, 6, 1, 2, 3, 7, 0];
+
+  const expected = 4; 
+  
+  const result = missingReindeer(input);
+
+  expect(result).toBe(expected);
+});
+
+test('should return 2 for the last one', () => {
+  const input = [0, 1];
+  // 2 (it's the last one that's missing!)
+  const expected = 2; 
+  
+  const result = missingReindeer(input);
+
+  expect(result).toBe(expected);
+});
+
+test('should return 2', () => {
+  const input = [3, 0, 1];
+
+  const expected = 2; 
+  
+  const result = missingReindeer(input);
+
+  expect(result).toBe(expected);
+});
