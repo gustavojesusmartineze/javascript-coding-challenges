c8ee5a132722d56e80de09adbd99f6d64d978601
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-10-15 07:51:29 -0400

    feat(countPackages): add edge case test for exercise

--- Exercises/17-CountPackages/countPackages.test.js
+++ Exercises/17-CountPackages/countPackages.test.js
@@ -16,3 +16,23 @@ test('should return 9', () => {
 
   expect(result).toBe(expected);
 });
+
+test('should return 15', () => {
+  const carriers = [
+    ['lolivier', 8, ['camila', 'jesuspoleo']],
+    ['camila', 5, ['sergiomartinez', 'conchaasensio']],
+    ['jesuspoleo', 4, []],
+    ['sergiomartinez', 4, []],
+    ['conchaasensio', 3, ['facundocapua', 'faviola']],
+    ['facundocapua', 2, []],
+    ['faviola', 1, []]
+  ];
+
+  const carrierID = 'camila';
+
+  const expected = 15; 
+
+  const result = countPackages(carriers, carrierID);
+
+  expect(result).toBe(expected);
+});
