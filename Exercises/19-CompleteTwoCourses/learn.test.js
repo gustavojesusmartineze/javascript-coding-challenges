5ac996f4a0bc8e530f606f7ecd07a4ce96de55a0
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-10-16 08:01:50 -0400

    feat(learn): add edge case test for exercise

--- Exercises/19-CompleteTwoCourses/learn.test.js
+++ Exercises/19-CompleteTwoCourses/learn.test.js
@@ -48,3 +48,39 @@ test('should return [1, 2]', () => {
 
   expect(result).toStrictEqual(expected);
 });
+
+test('should return [3, 4]', () => {
+  const courses = [8, 2, 1, 4, 3];
+  const time = 8;
+
+  // we use the maximum time available so [3, 4] uses 7 hours and the [1, 2] would only use 3 hours.
+  const expected = [3, 4]; 
+  
+  const result = learn(time, courses);
+
+  expect(result).toStrictEqual(expected);
+});
+
+test('should return null', () => {
+  const courses = [10, 14, 20];
+  const time = 4;
+
+  // null -> we don't have time to do two courses
+  const expected = null; 
+  
+  const result = learn(time, courses);
+
+  expect(result).toStrictEqual(expected);
+});
+
+test('should return null', () => {
+  const courses = [5, 5, 5];
+  const time = 5;
+
+  // null -> we don't have time to do two courses
+  const expected = null; 
+  
+  const result = learn(time, courses);
+
+  expect(result).toStrictEqual(expected);
+});
