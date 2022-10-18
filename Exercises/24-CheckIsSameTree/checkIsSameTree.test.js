723bede03edc97682206e5c69493ca0f751958d9
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-10-18 15:35:51 -0400

    feat(checkIsSameTree): add edge case test for exercise

--- Exercises/24-CheckIsSameTree/checkIsSameTree.test.js
+++ Exercises/24-CheckIsSameTree/checkIsSameTree.test.js
@@ -14,3 +14,23 @@ test('should return true for treeA, treeA', () => {
 
   expect(result).toBe(expected);
 });
+
+test('should return false for treeA, treeB', () => {
+  const treeA = {
+    value: 1,
+    left: { value: 2, left: null, right: null },
+    right: { value: 3, left: null, right: null }
+  };
+
+  const treeB = {
+    value: 1,
+    left: { value: 3, left: { value: 2, left: null, right: null }, right: null },
+    right: { value: 5, left: null, right: { value: 4, left: null, right: null } }
+  }
+
+  const expected = false; 
+  
+  const result = checkIsSameTree(treeA, treeB);
+
+  expect(result).toBe(expected);
+});
