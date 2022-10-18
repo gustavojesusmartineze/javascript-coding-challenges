c7eee6331303a087334c0be926981b8315fd7449
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-10-18 15:33:36 -0400

    feat(canReconfigure): add edge case test for exercise

--- Exercises/23-CanReconfigureFactory/canReconfigure.test.js
+++ Exercises/23-CanReconfigureFactory/canReconfigure.test.js
@@ -22,3 +22,36 @@ test('should return false', () => {
 
   expect(result).toBe(expected);
 });
+
+test('should return false case 1', () => {
+  const from = 'XBOX'
+  const to = 'XXBO'
+
+  const expected = false; 
+  
+  const result = canReconfigure(from, to);
+
+  expect(result).toBe(expected);
+});
+
+test('should return true case 1', () => {
+  const from = 'XBOX'
+  const to = 'XOBX'
+
+  const expected = true; 
+  
+  const result = canReconfigure(from, to);
+
+  expect(result).toBe(expected);
+});
+
+test('should return false case 2', () => {
+  const from = 'MMM'
+  const to = 'MID'
+
+  const expected = false; 
+  
+  const result = canReconfigure(from, to);
+
+  expect(result).toBe(expected);
+});
