cd1c5afd90d053b4d8cd4fe07e041af687ddfcb7
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-10-21 20:27:55 -0400

    feat(fibonacci): add edge case test for exercise

--- mouredev/03-Fibonacci/fibonacci.test.js
+++ mouredev/03-Fibonacci/fibonacci.test.js
@@ -27,3 +27,30 @@ test('should return 3 for fibonacci of 4', () => {
 
   expect(result).toBe(expected);
 });
+
+test('should return 55 for fibonacci of 10', () => {
+  const number = 10;
+  const expected = 55;
+
+  const result = fibonacci(number);
+
+  expect(result).toBe(expected);
+});
+
+test('should return 610 for fibonacci of 15', () => {
+  const number = 15;
+  const expected = 610;
+
+  const result = fibonacci(number);
+
+  expect(result).toBe(expected);
+});
+
+test('should return 6765 for fibonacci of 20', () => {
+  const number = 20;
+  const expected = 6765;
+
+  const result = fibonacci(number);
+
+  expect(result).toBe(expected);
+});
